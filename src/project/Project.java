c025ba9ad0893948bb69f227624a9630585ecbab
akg2000 <aman.gupta1271@gmail.com>
2019-10-20 13:24:58 +0530

    dd

--- src/project/Project.java
+++ src/project/Project.java
@@ -22,7 +22,7 @@ public class Project extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        ///123
         stage.setScene(scene);
         stage.show();
     }
