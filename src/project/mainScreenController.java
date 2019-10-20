7b057b7742443bc30172d43be0e32cff1fef6810
broompa <56160134+broompa@users.noreply.github.com>
2019-10-20 15:14:43 +0530

    Transition

--- src/project/mainScreenController.java
+++ src/project/mainScreenController.java
@@ -6,15 +6,61 @@
 package project;
 
 import java.io.IOException;
-import javafx.event.ActionEvent;
 
-/**
- * FXML Controller class
- *
- * @author Asus
- */
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+
+
+
 public class mainScreenController {
+    
+    
+    @FXML
+    private StackPane container;
+    
+    @FXML
+    private AnchorPane anchorRoot;
+    
+    
+    @FXML
+    private Button newGameButton;
+    
+    
+    
+    
+    
+    
+    
     public void newGame(ActionEvent event) throws IOException {
+                Parent root = FXMLLoader.load(getClass().getResource("userDetails.fxml"));
+                Scene sc = newGameButton.getScene();
+                root.translateYProperty().set(sc.getHeight());
+                
+                container.getChildren().add(root);
+                Timeline t = new Timeline();
+                
+                KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                
+                t.getKeyFrames().add(kf);
+                t.setOnFinished(t1->{
+                   container.getChildren().remove(anchorRoot);
+                });
+                t.play();
+                
             
 		
 	}
