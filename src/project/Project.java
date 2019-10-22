313c2ea0a0e4d53c43e8edb42a1448dadbdcd63b
akg2000 <aman.gupta1271@gmail.com>
2019-10-22 15:23:46 +0530

    changes

--- src/project/Project.java
+++ src/project/Project.java
@@ -20,7 +20,7 @@ public class Project extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
-        
+        User current_user;
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
--- src/project/resumeGameController.java
+++ src/project/resumeGameController.java
@@ -6,16 +6,21 @@
 package project;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
@@ -25,17 +30,21 @@ import javafx.util.Duration;
  *
  * @author Asus
  */
-public class resumeGameController  {
-     @FXML
+public class resumeGameController {
+    String[] arr;
+    @FXML
     private StackPane container;
-    
     @FXML
     private AnchorPane anchorRoot;
-  
-    
+    @FXML
+    private ComboBox chooseUser; 
     @FXML
     private Button userScreenBack;
-
+    public resumeGameController() {
+        arr = new String[20];
+        arr = User.getPlayerList();
+        chooseUser = new ComboBox(FXCollections.observableArrayList(arr));
+    }
    public void user_back(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
         Scene sc = userScreenBack.getScene();
