8cbb1205a843ac4f151426d90769e7f9ebcc382a
akg2000 <aman.gupta1271@gmail.com>
2019-11-01 23:10:41 +0530

    design changes

--- src/project/mainScreenController.java
+++ src/project/mainScreenController.java
@@ -11,12 +11,13 @@ import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
@@ -25,7 +26,7 @@ import javafx.util.Duration;
 
 
 
-public class mainScreenController {
+public class mainScreenController{
     
     
     @FXML
@@ -36,80 +37,138 @@ public class mainScreenController {
     
     
     @FXML
-    private Button newGameButton;
+    private ImageView newgameButton;
+    @FXML
+    private ImageView resumegameButton;
+    @FXML
+    private ImageView almanacButton;
+    @FXML
+    private ImageView leaderboardButton;
+    @FXML
+    private ImageView exitButton;
+    @FXML
+    private ImageView creditsButton;
     
     
     
-    
-    
-    
-    
-    public void newGame(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("userDetails.fxml"));
-        Scene sc = newGameButton.getScene();
-        root.translateYProperty().set(sc.getHeight());
-        container.getChildren().add(root);
-        Timeline t = new Timeline();
-        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-        t.getKeyFrames().add(kf);
-        t.setOnFinished(t1->{
-           container.getChildren().remove(anchorRoot);
+    public void newGame() throws IOException{
+        newgameButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("userDetails.fxml"));
+                    Scene sc = newgameButton.getScene();
+                    root.translateYProperty().set(sc.getHeight());
+                    container.getChildren().add(root);
+                    Timeline t = new Timeline();
+                    KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                    KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                    t.getKeyFrames().add(kf);
+                    t.setOnFinished(t1->{
+                        container.getChildren().remove(anchorRoot);
+                    });
+                    t.play();
+                }catch (IOException ex) {
+                    System.out.println("IO Error");
+                    System.exit(0);
+                }
+                }
         });
-        t.play();
     }
-    public void resumeGamebtn(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("resumeGame.fxml"));
-        Scene sc = newGameButton.getScene();
-        root.translateYProperty().set(sc.getHeight());
-        container.getChildren().add(root);
-        Timeline t = new Timeline();
-        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-        t.getKeyFrames().add(kf);
-        t.setOnFinished(t1->{
-           container.getChildren().remove(anchorRoot);
+    public void resumeGame() throws IOException{
+        resumegameButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("resumeGame.fxml"));
+                    Scene sc = resumegameButton.getScene();
+                    root.translateYProperty().set(sc.getHeight());
+                    container.getChildren().add(root);
+                    Timeline t = new Timeline();
+                    KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                    KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                    t.getKeyFrames().add(kf);
+                    t.setOnFinished(t1->{
+                        container.getChildren().remove(anchorRoot);
+                    });
+                    t.play();
+                }catch (IOException ex) {
+                    System.out.println("IO Error");
+                    System.exit(0);
+                }
+                }
         });
-        t.play();
-	}
-    public void exitGame(ActionEvent event) throws IOException {
-        System.exit(0);
-		
-	}
-    public void almanac(ActionEvent event) throws IOException {
-		
-		
-	}
-    public void credits(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("creditsScreen.fxml"));
-        Scene sc = newGameButton.getScene();
-        root.translateYProperty().set(sc.getHeight());
-        container.getChildren().add(root);
-        Timeline t = new Timeline();
-        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-        t.getKeyFrames().add(kf);
-        t.setOnFinished(t1->{
-           container.getChildren().remove(anchorRoot);
+    }
+    
+    public void exitGame() throws IOException{
+        exitButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(0);
+            }
         });
-        t.play();
-		
-	}
-    public void leaderboard(ActionEvent event) throws IOException {
-	Parent root = FXMLLoader.load(getClass().getResource("leaderBoardScreen.fxml"));
-        Scene sc = newGameButton.getScene();
-        root.translateYProperty().set(sc.getHeight());
-        container.getChildren().add(root);
-        Timeline t = new Timeline();
-        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-        t.getKeyFrames().add(kf);
-        t.setOnFinished(t1->{
-           container.getChildren().remove(anchorRoot);
+    }
+    public void almanac() throws IOException {
+	almanacButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(0);
+            }
         });
-        t.play();	
-		
-	}
+    }	
+	
+    public void credits() throws IOException{
+        creditsButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("creditsScreen.fxml"));
+                    Scene sc = creditsButton.getScene();
+                    root.translateYProperty().set(sc.getHeight());
+                    container.getChildren().add(root);
+                    Timeline t = new Timeline();
+                    KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                    KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                    t.getKeyFrames().add(kf);
+                    t.setOnFinished(t1->{
+                        container.getChildren().remove(anchorRoot);
+                    });
+                    t.play();
+                }catch (IOException ex) {
+                    System.out.println("IO Error");
+                    System.exit(0);
+                }
+                }
+        });
+    }
+    public void leaderboard() throws IOException{
+        leaderboardButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("leaderBoardScreen.fxml"));
+                    Scene sc = leaderboardButton.getScene();
+                    root.translateYProperty().set(sc.getHeight());
+                    container.getChildren().add(root);
+                    Timeline t = new Timeline();
+                    KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                    KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                    t.getKeyFrames().add(kf);
+                    t.setOnFinished(t1->{
+                        container.getChildren().remove(anchorRoot);
+                    });
+                    t.play();
+                }catch (IOException ex) {
+                    System.out.println("IO Error");
+                    System.exit(0);
+                }
+                }
+        });
+    }
     
     
 }
--- src/project/userDetailsController.java
+++ src/project/userDetailsController.java
@@ -1,26 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
@@ -35,7 +29,7 @@ public class userDetailsController {
     
     
     @FXML
-    private Button userScreenBack;
+    private ImageView backButton;
     
     @FXML
     private TextField textFieldUsername;
@@ -44,76 +38,92 @@ public class userDetailsController {
     private TextField textFieldAge;
     
     @FXML 
-    private Button userProceed;
+    private ImageView proceedButton;
     
     @FXML
     private Label userScreenLabel;
 
-   public void user_back(ActionEvent event) throws IOException {
-          Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
-        Scene sc = userScreenBack.getScene();
-        root.translateYProperty().set(-sc.getHeight());
-                
-        container.getChildren().add(root);
-        Timeline t = new Timeline();
-                
-        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-                
-        t.getKeyFrames().add(kf);
-        t.setOnFinished(t1->{
-            container.getChildren().remove(anchorRoot);
+    public void user_back() throws IOException{
+        backButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+                    Scene sc = backButton.getScene();
+                    root.translateYProperty().set(sc.getHeight());
+                    container.getChildren().add(root);
+                    Timeline t = new Timeline();
+                    KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                    KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                    t.getKeyFrames().add(kf);
+                    t.setOnFinished(t1->{
+                        container.getChildren().remove(anchorRoot);
+                    });
+                    t.play();
+                }catch (IOException ex) {
+                    System.out.println("IO Error");
+                    System.exit(0);
+                }
+                }
         });
-        t.play();
     }
-   
-   public void user_proceed(ActionEvent event) throws IOException{
-       int age=0;
-       try{
-           age=Integer.parseInt(textFieldAge.getText());
-           if (textFieldUsername.getText().isEmpty()|| textFieldUsername.getText().toCharArray().length==0) {
-                userScreenLabel.setText("Username cannot be empty");
-                textFieldUsername.setText("");
-                textFieldAge.setText("");
+   public void user_proceed() throws IOException{
+        proceedButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+            try{
+                int age=Integer.parseInt(textFieldAge.getText());
+                if (textFieldUsername.getText().isEmpty()|| textFieldUsername.getText().toCharArray().length==0) {
+                    userScreenLabel.setText("Username cannot be empty");
+                    textFieldUsername.setText("");
+                    textFieldAge.setText("");
+                }
+                else if(textFieldAge.getText().isEmpty()|| textFieldAge.getText().length()==0){
+                    userScreenLabel.setText("Age cannot be empty");
+                    textFieldUsername.setText("");
+                    textFieldAge.setText("");
+                }
+                else if(age>100){
+                    userScreenLabel.setText("Age cannot be more than 100");
+                    textFieldUsername.setText("");
+                    textFieldAge.setText("");
+                }
+                else if (User.doesExists(textFieldUsername.getText())){
+                    userScreenLabel.setText("Already Exists, choose another name.");
+                    textFieldUsername.setText("");
+                    textFieldAge.setText("");
+                }
+                else {
+                    userScreenLabel.setText("User Creation Succesful.");
+                    Parent root;
+                    try {
+                        root = FXMLLoader.load(getClass().getResource("level1.fxml"));
+                        Scene sc = backButton.getScene();
+                        root.translateYProperty().set(sc.getHeight());
+                        container.getChildren().add(root);
+                        Timeline t = new Timeline();
+                        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                        t.getKeyFrames().add(kf);
+                        t.setOnFinished(t1->{
+                            container.getChildren().remove(anchorRoot);
+                        });
+                        t.play();
+                        Project.setUser(new User(textFieldUsername.getText()));
+                    } catch (IOException ex) {
+                        System.out.println("IO Error");
+                        System.exit(0);
+                    }
+                }
             }
-            else if(textFieldAge.getText().isEmpty()|| textFieldAge.getText().length()==0){
-                userScreenLabel.setText("Age cannot be empty");
-                textFieldUsername.setText("");
+            catch(NumberFormatException e){
+                userScreenLabel.setText("Age has to be a number");
                 textFieldAge.setText("");
-            }
-            else if(age>100){
-                userScreenLabel.setText("Age cannot be more than 100");
-                textFieldUsername.setText("");
                 textFieldAge.setText("");
-            }
-            else if (User.doesExists(textFieldUsername.getText())){
-                userScreenLabel.setText("Already Exists, choose another name.");
-                textFieldUsername.setText("");
-                textFieldAge.setText("");
-            }
-            else {
-                userScreenLabel.setText("User Creation Succesful.");
-                Parent root = FXMLLoader.load(getClass().getResource("level1.fxml"));
-                Scene sc = userScreenBack.getScene();
-                root.translateYProperty().set(sc.getHeight());
-                container.getChildren().add(root);
-                Timeline t = new Timeline();
-                KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-                KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-                t.getKeyFrames().add(kf);
-                t.setOnFinished(t1->{
-                    container.getChildren().remove(anchorRoot);
-                });
-                t.play();
-                Project.setUser(new User(textFieldUsername.getText()));
-                
             }
         }
-       catch(NumberFormatException e){
-           userScreenLabel.setText("Age has to be a number");
-           textFieldAge.setText("");
-           textFieldAge.setText("");
+    });
     }
-   }
 }
 
