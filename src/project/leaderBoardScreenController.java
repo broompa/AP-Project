5d77219d8c1ddeb4a6152238e5e9c8a6117cff7b
akg2000 <aman.gupta1271@gmail.com>
2019-11-02 18:30:59 +0530

    resized all windows

--- src/project/leaderBoardScreenController.java
+++ src/project/leaderBoardScreenController.java
@@ -12,11 +12,14 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
@@ -34,26 +37,32 @@ public class leaderBoardScreenController  {
     
     
     @FXML
-    private Button userScreenBack;
+    private ImageView backButton;
 
-    public void user_back(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
-        Scene sc = userScreenBack.getScene();
-        root.translateYProperty().set(-sc.getHeight());
-                
-        container.getChildren().add(root);
-        Timeline t = new Timeline();
-                
-        KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
-        KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
-                
-        t.getKeyFrames().add(kf);
-        t.setOnFinished(t1->{
-            container.getChildren().remove(anchorRoot);
+    public void user_back() throws IOException{
+        backButton.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+                    Scene sc = backButton.getScene();
+                    root.translateYProperty().set(-sc.getHeight());
+                    container.getChildren().add(root);
+                    Timeline t = new Timeline();
+                    KeyValue kv = new KeyValue(root.translateYProperty(),0,Interpolator.EASE_IN);
+                    KeyFrame kf = new KeyFrame(Duration.millis(1000),kv);
+                    t.getKeyFrames().add(kf);
+                    t.setOnFinished(t1->{
+                        container.getChildren().remove(anchorRoot);
+                    });
+                    t.play();
+                }catch (IOException ex) {
+                    System.out.println("IO Error");
+                    System.exit(0);
+                }
+                }
         });
-        t.play();
-    
-
     }
 
 }
